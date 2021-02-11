1ed25bffa26a83591d66625778fe73dec29fece0
ruddy palvair <r.palvair@gmail.com>
2021-02-11 15:44:24 +0100

    feat : Create elements without JSX

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,12 +1,17 @@
-import { Component } from "react"
+import React, { Component } from "react"
 import "./App.css"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>React App</h1>
-      </div>
+      // <div className="App">
+      //   <h1>React App</h1>
+      // </div>
+      React.createElement(
+        "div",
+        { className: "App" },
+        React.createElement("h1", null, "React App")
+      )
     )
   }
 }
