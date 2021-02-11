6c1773a6f2fb9fe2b889103da0e3fa21c75672e4
ruddy palvair <r.palvair@gmail.com>
2021-02-11 16:01:25 +0100

    feat : Use Fragment for adjacent elements

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from "react"
+import React, { Component, Fragment } from "react"
 import "./App.css"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>React App</h1>
-      </div>
+      <Fragment>
+        <div class="App">
+          <h1>React App</h1>
+        </div>
+        <h2>Second title</h2>
+      </Fragment>
     )
   }
 }
