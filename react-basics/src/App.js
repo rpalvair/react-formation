cfcb99ae81a929904727cc3466cbfa56f191dddf
ruddy palvair <r.palvair@gmail.com>
2021-02-11 15:44:57 +0100

    feat : Let's use JSX

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -4,14 +4,9 @@ import "./App.css"
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      //   <h1>React App</h1>
-      // </div>
-      React.createElement(
-        "div",
-        { className: "App" },
-        React.createElement("h1", null, "React App")
-      )
+      <div className="App">
+        <h1>React App</h1>
+      </div>
     )
   }
 }
