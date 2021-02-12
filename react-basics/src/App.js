911691c142db84b0a3e494726251dd5cc2b18e87
ruddy palvair <r.palvair@gmail.com>
2021-02-12 12:20:17 +0100

    feat : Copy state before modify then update

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -26,7 +26,11 @@ class App extends Component {
   }
 
   handleClick = () => {
-    console.log("click")
+    const famille = { ...this.state.famille }
+    famille.membre1.age += 1
+    this.setState({
+      famille,
+    })
   }
 
   render() {
@@ -38,7 +42,9 @@ class App extends Component {
         <Membre age={famille.membre1.age} nom={famille.membre1.nom} />
         <Membre age={famille.membre2.age} nom={famille.membre2.nom} />
         <Membre age={famille.membre3.age} nom={famille.membre3.nom} />
-        <Membre age={famille.membre4.age} nom={famille.membre4.nom}>Je suis la plus grande</Membre>
+        <Membre age={famille.membre4.age} nom={famille.membre4.nom}>
+          Je suis la plus grande
+        </Membre>
         <button onClick={this.handleClick}>Vieillir</button>
       </div>
     )
