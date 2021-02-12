c4667617da0bf7fee1b8bc0ea98ca446b5d3de57
ruddy palvair <r.palvair@gmail.com>
2021-02-12 11:57:16 +0100

    feat : Add object in state

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -2,7 +2,29 @@ import React, { Component, Fragment } from "react"
 import "./App.css"
 import Membre from "./components/Membre"
 
+const famille = {
+  membre1: {
+    nom: "Ruddy",
+    age: 35,
+  },
+  membre2: {
+    nom: "Andy",
+    age: 32,
+  },
+  membre3: {
+    nom: "Willy",
+    age: 28,
+  },
+  membre4: {
+    nom: "Jessie",
+    age: 36,
+  },
+}
 class App extends Component {
+  state = {
+    famille,
+  }
+
   render() {
     const { titre } = this.props
     return (
@@ -14,9 +36,7 @@ class App extends Component {
         <Membre nom="Ruddy" />
         <Membre nom="Andy" />
         <Membre nom="Willy" />
-        <Membre nom="Jessie" >
-          Je suis la plus grande
-        </Membre>
+        <Membre nom="Jessie">Je suis la plus grande</Membre>
       </Fragment>
     )
   }
