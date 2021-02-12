cc281b2de19df0cf2545b867d6da7574ac3657b9
ruddy palvair <r.palvair@gmail.com>
2021-02-12 11:23:39 +0100

    feat : Import property with destructuring in class component

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -4,10 +4,11 @@ import Membre from "./components/Membre"
 
 class App extends Component {
   render() {
+    const { titre } = this.props
     return (
       <Fragment>
         <div class="App">
-          <h1>{this.props.titre}</h1>
+          <h1>{titre}</h1>
         </div>
         <h2>Second titre</h2>
         <Membre nom="Ruddy" />
