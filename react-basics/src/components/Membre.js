66d1a2ed601bf132f13411ac1e09af604880b401
ruddy palvair <r.palvair@gmail.com>
2021-03-01 11:24:41 +0100

    feat : Inline css with conditions

--- react-basics/src/components/Membre.js
+++ react-basics/src/components/Membre.js
@@ -3,7 +3,9 @@ import React, { Fragment } from "react"
 const Membre = ({ nom, age, children }) => {
   return (
     <Fragment>
-      <h2>{nom.toUpperCase()}: {age} ans</h2>
+      <h2 style={{ backgroundColor: age < 30 ? "purple" : "yellow", color: "white" }}>
+        {nom.toUpperCase()}: {age} ans
+      </h2>
       {children ? <p>{children}</p> : <Fragment />}
     </Fragment>
   )
