499ccf470b5fd779e7a5f08f9802253d57189f88
ruddy palvair <r.palvair@gmail.com>
2021-03-01 12:34:18 +0100

    feat : Bind text-area with state

--- markdown-app/src/App.js
+++ markdown-app/src/App.js
@@ -3,18 +3,22 @@ import "./App.css"
 import { sampleText } from "./sampleText"
 
 class App extends Component {
+  state = {
+    text: sampleText,
+  }
   render() {
+    const { text } = this.state
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
             <textarea
-              value={sampleText}
+              value={text}
               className="form-control"
               rows="35"
             ></textarea>
           </div>
-          <div className="col-sm-6">{sampleText}</div>
+          <div className="col-sm-6">{text}</div>
         </div>
       </div>
     )
