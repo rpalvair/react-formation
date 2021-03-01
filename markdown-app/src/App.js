0522923e738abb4d896dd1cd7068afc2dce46a50
ruddy palvair <r.palvair@gmail.com>
2021-03-01 12:37:07 +0100

    feat : Update state dynamically

--- markdown-app/src/App.js
+++ markdown-app/src/App.js
@@ -6,6 +6,13 @@ class App extends Component {
   state = {
     text: sampleText,
   }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
   render() {
     const { text } = this.state
     return (
@@ -13,6 +20,7 @@ class App extends Component {
         <div className="row">
           <div className="col-sm-6">
             <textarea
+              onChange={this.handleChange}
               value={text}
               className="form-control"
               rows="35"
