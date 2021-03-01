febeb72c16afda97a530abe63427c29f83817f47
ruddy palvair <r.palvair@gmail.com>
2021-03-01 12:29:35 +0100

    feat : Transform in class component

--- markdown-app/src/App.js
+++ markdown-app/src/App.js
@@ -1,21 +1,24 @@
+import { Component } from "react"
 import "./App.css"
 import { sampleText } from "./sampleText"
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6">
-          <textarea
-            value={sampleText}
-            className="form-control"
-            rows="35"
-          ></textarea>
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea
+              value={sampleText}
+              className="form-control"
+              rows="35"
+            ></textarea>
+          </div>
+          <div className="col-sm-6">{sampleText}</div>
         </div>
-        <div className="col-sm-6">{sampleText}</div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
