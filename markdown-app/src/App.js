9b2d8b3268f84e3f42711938f29449a569b1b591
ruddy palvair <r.palvair@gmail.com>
2021-03-01 12:26:48 +0100

    feat : Display sample text in right column

--- markdown-app/src/App.js
+++ markdown-app/src/App.js
@@ -6,11 +6,13 @@ function App() {
     <div className="container">
       <div className="row">
         <div className="col-sm-6">
-          <textarea value={sampleText} className="form-control" rows="35"></textarea>
+          <textarea
+            value={sampleText}
+            className="form-control"
+            rows="35"
+          ></textarea>
         </div>
-        <div className="col-sm-6">
-          <h1>Résultat</h1>
-        </div>
+        <div className="col-sm-6">{sampleText}</div>
       </div>
     </div>
   )
