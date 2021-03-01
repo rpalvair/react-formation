5cdaec13328ad4e125a32e8735d4148430d8c74c
ruddy palvair <r.palvair@gmail.com>
2021-03-01 12:23:56 +0100

    feat : Import sample text

--- markdown-app/src/App.js
+++ markdown-app/src/App.js
@@ -1,19 +1,19 @@
 import "./App.css"
+import { sampleText } from "./sampleText"
 
 function App() {
   return (
-  <div className="container">
-    <div className="row">
-      <div className="col-sm-6">
-        <textarea className="form-control" rows="35"></textarea>
-      </div>
-      <div className="col-sm-6">
-        <h1>Résultat</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <textarea value={sampleText} className="form-control" rows="35"></textarea>
+        </div>
+        <div className="col-sm-6">
+          <h1>Résultat</h1>
+        </div>
       </div>
     </div>
-  </div>
   )
-
 }
 
 export default App
