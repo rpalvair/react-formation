f6ab96d0bfaa67862e14476452b3f8a9fb684bfc
ruddy palvair <r.palvair@gmail.com>
2021-03-02 15:16:37 +0100

    feat : Redirect to chat with dynamic url

--- chatbox-app/src/components/Connexion.js
+++ chatbox-app/src/components/Connexion.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react"
+import { Redirect } from "react-router-dom"
 
 class Connexion extends Component {
+  state = {
+    pseudo: "",
+    goToChat: false,
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      pseudo: event.target.value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      goToChat: true,
+    })
+  }
+
   render() {
+    if (this.state.goToChat) {
+      return <Redirect to={`/pseudo/${this.state.pseudo}`}></Redirect>
+    }
+
     return (
       <div className="connexionBox">
-        <form className="connexion">
-          <input placeholder="Pseudo" type="text" required />
+        <form className="connexion" onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.pseudo}
+            onChange={this.handleChange}
+            placeholder="Pseudo"
+            type="text"
+            required
+          />
           <button type="submit">GO</button>
         </form>
       </div>
