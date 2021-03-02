b5d77fba2d5e6fd4464350f1997529f5d2faf0d2
ruddy palvair <r.palvair@gmail.com>
2021-03-02 16:20:27 +0100

    feat : Save messages in state

--- chatbox-app/src/App.js
+++ chatbox-app/src/App.js
@@ -4,13 +4,26 @@ import Formulaire from "./components/Formulaire"
 import Message from "./components/Message"
 
 class App extends Component {
+  state = {
+    messages: {},
+    pseudo: this.props.match.params.pseudo
+  }
+
+  addMessage = (message) => {
+    const messages = { ...this.state.messages }
+    messages[`message-${Date.now()}`] = message
+    this.setState({
+      messages
+    })
+  }
+
   render() {
     return (
       <div className="box">
         <div>
           <Message></Message>
         </div>
-        <Formulaire></Formulaire>
+        <Formulaire pseudo={this.state.pseudo} addMessage={this.addMessage}></Formulaire>
       </div>
     )
   }
--- chatbox-app/src/components/Formulaire.js
+++ chatbox-app/src/components/Formulaire.js
@@ -1,10 +1,43 @@
 import { Component } from "react"
 
 class Formulaire extends Component {
+  state = {
+    message: "",
+  }
+
+  createMessage = () => {
+    const { addMessage, pseudo } = this.props
+    const message = {
+      pseudo,
+      message: this.state.message,
+    }
+    console.log(message)
+    addMessage(message)
+    this.setState({
+      message: "",
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.createMessage()
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      message: event.target.value,
+    })
+  }
+
   render() {
     return (
-      <form className="form">
-        <textarea required maxLength="140"></textarea>
+      <form className="form" onSubmit={this.handleSubmit}>
+        <textarea
+          value={this.state.message}
+          onChange={this.handleChange}
+          required
+          maxLength="140"
+        ></textarea>
         <div className="info">140</div>
         <button type="submit">Envoyer!</button>
       </form>
