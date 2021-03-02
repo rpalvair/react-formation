9989a916f107413fbd9d3894f8fb009f838ba1ed
ruddy palvair <r.palvair@gmail.com>
2021-03-02 14:58:29 +0100

    feat : Use router

--- chatbox-app/src/index.js
+++ chatbox-app/src/index.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
 import Connexion from "./components/Connexion"
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals"
+
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+
+const Root = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/"  component={Connexion}></Route>
+    </Switch>
+  </BrowserRouter>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Connexion />
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
