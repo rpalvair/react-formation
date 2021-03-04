6f5d274d3965615c08ed4d0945b597318e608301
ruddy palvair <r.palvair@gmail.com>
2021-03-04 11:22:51 +0100

    feat : Form to add recipe

--- recipes-app/src/components/AjouterRecette.js
+++ recipes-app/src/components/AjouterRecette.js
@@ -1,8 +1,55 @@
 import React, { Component } from "react"
 
 class AjouterRecette extends Component {
+  state = {
+    nom: "",
+    image: "",
+    ingredients: "",
+    instructions: "",
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value,
+    })
+  }
+
   render() {
-    return <h2>Ajouter recette</h2>
+    return (
+      <div className="card">
+        <form className="admin-form ajouter-recette">
+          <input
+            value={this.state.nom}
+            name="nom"
+            type="text"
+            placeholder="Nom de la recette"
+            onChange={this.handleChange}
+          ></input>
+          <input
+            value={this.state.image}
+            name="image"
+            type="text"
+            placeholder="Nom de l'image"
+            onChange={this.handleChange}
+          ></input>
+          <textarea
+            value={this.state.ingredients}
+            name="ingredients"
+            rows="3"
+            placeholder="Liste des ingrédients"
+            onChange={this.handleChange}
+          ></textarea>
+          <textarea
+            value={this.state.instructions}
+            name="instructions"
+            rows="15"
+            placeholder="Liste des instructions"
+            onChange={this.handleChange}
+          ></textarea>
+        </form>
+      </div>
+    )
   }
 }
 
