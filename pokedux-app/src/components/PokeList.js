fb90ca7127ac36bcc6a85beb7b024b0eb8f01cc1
ruddy palvair <r.palvair@gmail.com>
2021-03-07 11:58:58 +0100

    feat : Install dependencies and init app

---
+++ pokedux-app/src/components/PokeList.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PokemonItem from "./PokemonItem"
+
+const PokeList = () => (
+  <div className="list-container">
+    <h2>Try : 0</h2>
+    <h2>Pokedex</h2>
+    <ul>
+      <PokemonItem />
+      <PokemonItem />
+      <PokemonItem />
+    </ul>
+  </div>
+)
+
+export default PokeList
---
+++ pokedux-app/src/components/PokemonItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const PokemonItem = () => (
+  <li className="pokemon-item">
+    <img
+     alt={"ditto"}
+     src={"https://raw.githunusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"}></img>
+     ditto
+  </li>
+)
+
+export default PokemonItem
