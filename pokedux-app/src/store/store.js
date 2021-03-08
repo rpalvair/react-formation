681426aa8c692237d7d503c0f9efd78804a4bd37
ruddy palvair <r.palvair@gmail.com>
2021-03-08 11:29:34 +0100

    feat : Plug redux-thunk

--- pokedux-app/src/store/store.js
+++ pokedux-app/src/store/store.js
@@ -1,9 +1,10 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
 import reducer from "./reducer"
 import initialState from "./initialState"
 import { Provider } from "react-redux"
 import React from "react"
+import thunk from "redux-thunk"
 
-const store = createStore(reducer, initialState)
+const store = createStore(reducer, initialState, applyMiddleware(thunk))
 
 export default (props) => <Provider store={store} {...props} />
